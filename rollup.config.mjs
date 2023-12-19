6a72db942be33fcb3db419284ed22eb408910dae
cheeselemon <cheeselemon.yarn@gmail.com>
2023-12-19 11:48:16 +0900

    feat: update ouptut settings

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 export default {
   input: 'src/index.ts',
   output: {
-    file: 'dist/bundle.js',
+    file: 'dist/index.js',
     format: 'es',
   },
   plugins: [
