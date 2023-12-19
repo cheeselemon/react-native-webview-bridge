d6e86d4432d2c8bd85f528bfb25a934a73720420
cheeselemon <cheeselemon.yarn@gmail.com>
2023-12-19 11:56:13 +0900

    feat: rename export

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import useReactNativeWebViewBridgeForReactNative from "./react-native/index";
 import useReactNativeWebViewBridgeForReact from "./react/index";
-const React = {
-  useReactNativeWebViewBridge: useReactNativeWebViewBridgeForReact,
-};
-const ReactNative = {
-  useReactNativeWebViewBridge: useReactNativeWebViewBridgeForReactNative,
-};
-const ReactNativeWebviewBridge = Object.assign({}, React, ReactNative);
-export default ReactNativeWebviewBridge;
+const React = useReactNativeWebViewBridgeForReact;
+const ReactNative = useReactNativeWebViewBridgeForReactNative;
+const useReactNativeWebviewBridge = Object.assign({}, React, ReactNative);
+export default useReactNativeWebviewBridge;
