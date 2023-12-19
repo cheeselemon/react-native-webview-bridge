4c5dbdb823cd32483b1b8c051200e043bd5f21df
cheeselemon <cheeselemon.yarn@gmail.com>
2023-12-19 14:20:01 +0900

    feat: update rollup config to handle cjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,14 +1,22 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import babel from "@rollup/plugin-babel";
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/index.ts',
   output: {
     file: 'dist/index.js',
     format: 'es',
+    sourcemap: true,
   },
   plugins: [
     nodeResolve(),
     typescript(),
+    commonjs(),
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+    }),
   ],
 };
