4cdcda8b18ac2b213488200928c9d73cc893d1ca
cheeselemon <cheeselemon.yarn@gmail.com>
2024-01-11 17:14:59 +0900

    fix: android old chrome mobile browser support

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,34 +1,39 @@
-import typescript from '@rollup/plugin-typescript';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
-import commonjs from '@rollup/plugin-commonjs';
-import strip from '@rollup/plugin-strip';
-
+import commonjs from "@rollup/plugin-commonjs";
+import strip from "@rollup/plugin-strip";
 
 // remove console log?
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
-    file: 'dist/index.js',
-    format: 'es',
+    file: "dist/index.js",
+    format: "es",
     sourcemap: true,
   },
-  external: ['react', 'react-dom'],
+  external: ["react", "react-dom"],
   plugins: [
     nodeResolve(),
     typescript(),
     commonjs(),
     babel({
       babelHelpers: "bundled",
-      presets: ["@babel/preset-env", "@babel/preset-react"],
+      presets: [
+        "@babel/preset-env",
+        "@babel/preset-react",
+        {
+          targets: "> 0.25%, not dead",
+          useBuiltIns: "usage",
+          corejs: 3,
+        },
+      ],
+      plugins: ["@babel/plugin-transform-optional-chaining"],
     }),
     strip({
-      include: [
-        '**/*.js',
-        '**/*.ts',
-      ],
-      debugger:true,
+      include: ["**/*.js", "**/*.ts"],
+      debugger: true,
       functions: ["console.log"],
     }),
   ],
--- src/react/index.ts
+++ src/react/index.ts
@@ -28,10 +28,14 @@ export default function useReactNativeWebViewBridge({
       }
     }
     console.log(`adding listener for ${RNWB_EVENT_NAME}`);
-    window?.addEventListener(RNWB_EVENT_NAME, handlerBridge, true);
+    if (window) {
+      window.addEventListener(RNWB_EVENT_NAME, handlerBridge, true);
+    }
     return () => {
       console.log(`removing listener for ${RNWB_EVENT_NAME}`);
-      window?.removeEventListener(RNWB_EVENT_NAME, handlerBridge, true);
+      if (window) {
+        window.removeEventListener(RNWB_EVENT_NAME, handlerBridge, true);
+      }
     };
   }, []);
 
